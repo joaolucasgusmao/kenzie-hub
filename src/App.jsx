5d65cfd890fa097bb8a4844ab419e20832144c40
João Lucas <joaolucasgusmao@hotmail.com>
2023-10-13 16:05:24 -0300

    feat: created style reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { DashboardPage } from "./pages/DashboardPage";
+import "./styles/index.scss";
 
 const App = () => {
   return (
     <>
-      {/* <LoginPage />
-      <RegisterPage /> */}
-      <DashboardPage />
+      <LoginPage />
+      {/* <RegisterPage />  */}
+      {/* <DashboardPage /> */}
     </>
   );
 };
