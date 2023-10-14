27e457d9ea3e3a96db0636ede0968902b5e681e0
João Lucas <joaolucasgusmao@hotmail.com>
2023-10-13 22:45:18 -0300

    feat: stylized dashboard

--- src/components/Dashboard/Header/index.jsx
+++ src/components/Dashboard/Header/index.jsx
@@ -1,8 +1,9 @@
 import Logo from "../../../assets/Logo.svg";
+import styles from "./style.module.scss";
 
 export const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <img src={Logo} alt="Logo" />
       <button className="btn exit">Sair</button>
     </header>
--- src/components/Dashboard/Main/index.jsx
+++ src/components/Dashboard/Main/index.jsx
@@ -1,11 +1,13 @@
+import styles from "./style.module.scss";
+
 export const Main = () => {
   return (
-    <main className="container">
-      <div>
+    <main className="container dashboard">
+      <div className={styles.topDiv}>
         <p className="title">Olá, João Lucas</p>
-        <p className="smText">Primeiro módulo (Introdução ao Frontend)</p>
+        <p className="smText dashboard">Primeiro módulo (Introdução ao Frontend)</p>
       </div>
-      <div>
+      <div className={styles.bottomDiv}>
         <p className="title">Que pena! Estamos em desenvolvimento :(</p>
         <p className="title text">
           Nossa aplicação está em desenvolvimento, em breve teremos novidades
